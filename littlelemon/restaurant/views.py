783ba526b7d6473926daf76acc50bc1a69396db1
ronitmajgaonkar <ronitmajgaonkar04@gmail.com>
2024-09-16 10:46:05 -0700

    Added authentication feature for tokens

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,24 +1,44 @@
 from django.shortcuts import render
-from rest_framework import generics, viewsets
-from .models import Menu, Booking
-from .serializers import menuSerializer, bookingSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import generics
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework import permissions, viewsets
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from .models import Menu, Booking
+from .serializers import menuSerializer, bookingSerializer, UserSerializer
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+
 def index(request):
     return render(request, 'index.html', {})
 
-class MenuItemsView(generics.ListCreateAPIView):
+class MenuItemView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = menuSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
-class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+class SingleMenuItemView(generics.RetrieveUpdateAPIView):
     queryset = Menu.objects.all()
     serializer_class = menuSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
+    
+class UserViewSet(viewsets.ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [permissions.IsAuthenticated]
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = bookingSerializer
-    permission_classes = [IsAuthenticated]
+   
+   
+@api_view()
+@permission_classes([IsAuthenticated])
+# @authentication_classes([TokenAuthentication])
+def msg(request):
+    return Response({"message":"This view is protected"})
